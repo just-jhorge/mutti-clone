f045cac2e413ad0f2a3be36a2ea69c45df4bb4cc
George Sarpong Afrifa <sarpongk247@gmail.com>
2023-11-01 14:55:50 +0000

    categories bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { IoMenuOutline } from "react-icons/io5";
+
+const categories = [
+    { label: "Baby & Child Health", value: "baby-and-child-health" },
+    { label: "Medicines & Treatment", value: "medicines-and-treatment" },
+    { label: "Sexual Health", value: "sexual-pleasure-and-wellbeing" },
+    { label: "Supplements", value: "vitamins-and-supplements" },
+    { label: "Women's Health", value: "women's-health" },
+];
+
 export default function Home() {
-  return (
-    <main>Mutti Web app clone</main>
-  )
+    return (
+        <main>
+            <section className="h-16 flex items-center justify-center text-sm">
+                <div className="container flex items-center gap-10">
+                    <div className="flex items-center gap-2">
+                        <IoMenuOutline size={23} />
+                        <p>All Categories</p>
+                    </div>
+                    <ul className="flex items-center gap-5">
+                        {categories.map((category) => (
+                            <li key={category.value}>
+                                <Link href={`/category/${category.value}`}>{category.label}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </section>
+        </main>
+    );
 }
