926ce214d1a04c7187827dbed8c0835a5c293965
George Sarpong Afrifa <sarpongk247@gmail.com>
2023-11-22 07:39:33 +0000

    updates and changes

--- src/app/(home)/category/[category]/subcategory/[subcategory]/page.tsx
+++ src/app/(home)/category/[category]/subcategory/[subcategory]/page.tsx
@@ -1,15 +1,3 @@
-import DrugCard from "@/app/(home)/components/DrugCard";
-
 export default function page() {
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
-            <DrugCard />
-            <DrugCard />
-            <DrugCard discountAvailable />
-            <DrugCard />
-            <DrugCard discountAvailable />
-            <DrugCard discountAvailable />
-            <DrugCard />
-        </div>
-    );
+    return <div className="grid grid-cols-1 md:grid-cols-4 gap-5">Some drugs go here</div>;
 }
